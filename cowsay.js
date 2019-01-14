e881896f5a2d0478e574d6d91c0108d1aa1bdd88
Taylor Sturtz <taylor.sturtz@ytel.com>
2019-01-13 16:51:21 -0800

    changed title, updated version, added links to blog etc

--- cowsay.js
+++ cowsay.js
@@ -2,6 +2,6 @@ const cowsay = require('cowsay');
 const colors = require('colors');
 
 module.exports = cowsay.think({
-	text: 'Taylor Sturtz'.bold.white + '\n' + 'Web Developer'.italic.cyan,
+	text: 'Taylor Sturtz'.bold.white + '\n' + 'Software Developer'.italic.cyan,
   f: 'tux'
 });
