c96538f5cd12edb2984b447f1a785b80d615bc66
Taylor Sturtz <taylorsturtz@gmail.com>
2020-05-12 21:58:29 -0700

    update job and other content

--- cowsay.js
+++ cowsay.js
@@ -2,6 +2,6 @@ const cowsay = require('cowsay');
 const colors = require('colors');
 
 module.exports = cowsay.think({
-	text: 'Taylor Sturtz'.bold.white + '\n' + 'Software Developer'.italic.cyan,
+	text: 'Taylor Sturtz'.bold.white + '\n' + 'Software Engineer'.italic.cyan,
   f: 'tux'
 });
